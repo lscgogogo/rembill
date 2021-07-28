1dfa84f2ab45a8f5f2797cb7135add1d17b13617
梁书聪 <8948760+liang-shucong@user.noreply.gitee.com>
2021-07-28 16:14:24 +0800

    点击标签可跳转页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,10 @@
 <template>
   <layout>
     <div class="tags">
-      <ol>
-        <li v-for="tag in tags" :key="tag.id">
+        <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
           <span>{{ tag.name }}</span>
           <icon name="right" />
-        </li>
-      </ol>
+        </router-link>
     </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
@@ -43,7 +41,7 @@ export default class Labels extends Vue {
   padding-left: 16px;
   font-size: 16px;
   background: white;
-  li {
+  .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
